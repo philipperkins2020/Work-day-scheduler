074dc0626c4206744ce3e3a570c24b1ec15594f8
PhilPerk <philipperkins2020@gmail.com>
2021-03-11 14:07:56 -0600

    Added color code and comments

--- planner.js
+++ planner.js
@@ -1,46 +1,87 @@
 console.log("helloworld")
-var hours = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
- 
-document.getElementById("currentDay").textContent=  moment().format('dddd');
 
- function buildPlanner(){
-    
-    for (var i=0; i<hours.length; i++){
-       
+//Array for hours in schedule planner
+var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
+//Grabbing the ID of currentDay to display day on page using the moment function
+document.getElementById("currentDay").textContent = moment().format('dddd');
+//Function for building the planner
+function buildPlanner() {
+    //Variable for grabbing the current hour in real time
+    var currentHour = moment().hour()
+    //Variable that sets isAfternoon to false
+    var isAfternoon = false
+    // if statement to 
+    if (currentHour > 12) {
+        isAfternoon = true
+    }
+    //For loop to run through every hour
+    for (var i = 0; i < hours.length; i++) {
+        // create an div element for the rows of time
         var row = document.createElement("div")
-        row.classList.add("row","time-block")
-        
+        //Added a classlist of row
+        row.classList.add("row", "time-block")
+        // Set the attribute for row to hours
+        row.setAttribute("id", hours[i])
         console.log(row)
-        var hour= document.createElement("div")
-        hour.classList.add("col-md-1","hour")
-        hour.innerHTML=hours[i]
+        // Variable for the hours
+        var hour = document.createElement("div")
+        // Added classlist for hour
+        hour.classList.add("col-md-1", "hour")
+        //Gave the hours html
+        hour.innerHTML = hours[i]
+        //Appended hour to row
         row.appendChild(hour)
+        // created text area element
         var textarea = document.createElement("textarea")
-        textarea.classList.add("col-md-10","toDo")
+        //Gave text area a class
+        textarea.classList.add("col-md-10", "toDo")
+        // Variable to parse the hours
+        let hourToAdd = parseInt(hours[i]);
+        //
+        if (isAfternoon && hourToAdd < 6){
+            hourToAdd+=12
+        }
+        // If the current hour is hourToAdd give the class of present
+        if (hourToAdd === currentHour) {
+           textarea.classList.add("present")
+
+        // If hour to add is less than currentHour give class of past
+        } else if(hourToAdd< currentHour) {
+            textarea.classList.add("past")
+        }
+        // If hour is not present or past class give it the future class
+        else {
+            textarea.classList.add("future")
+        }
+        //Append the textarea to row
         row.appendChild(textarea)
+        //Variable for button element
         var saveButton = document.createElement("button")
-        saveButton.classList.add("btn","col-md-1","saveBtn")
+        //Class for save button
+        saveButton.classList.add("btn", "col-md-1", "saveBtn")
+        //
         var iTag = document.createElement("i")
-        iTag.classList.add("fas","fa-save")
+        iTag.classList.add("fas", "fa-save")
+        //Appending iTag to save button
         saveButton.appendChild(iTag)
+        //Appending saveButton to row
         row.appendChild(saveButton)
-        
-      
-      
-      
-        
+        //Grabbing the id of planner and appending it to row
         document.getElementById("planner").appendChild(row)
-     }
- }
-
-
-function colorCode(){
-    console.log(hours)
+    }
 }
 
- function getHour() {
-     return moment().hour();
- }
+
+function colorCode() {
+    for (var i = 0; i < hours.length; i++) {
+
+    }
+
+}
+
+function getHour() {
+    return moment().hour();
+}
 
 
- buildPlanner()
+buildPlanner()
