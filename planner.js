c1ec34489fe3b9c62c6e2a7883fd56dec1f6dd35
PhilPerk <philipperkins2020@gmail.com>
2021-03-11 10:37:08 -0600

    added 9-5 time blocks

---
+++ planner.js
@@ -0,0 +1,34 @@
+console.log("helloworld")
+
+ document.getElementById("currentDay").textContent=  moment().format('dddd');
+
+ function buildPlanner(){
+    var hours = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"] 
+    for (var i=0; i<hours.length; i++){
+       
+        var row = document.createElement("div")
+        row.classList.add("row","time-block")
+        
+        console.log(row)
+        var hour= document.createElement("div")
+        hour.classList.add("col-md-1","hour")
+        hour.innerHTML=hours[i]
+        row.appendChild(hour)
+        var textarea = document.createElement("textarea")
+        textarea.classList.add("col-md-10","toDo")
+        row.appendChild(textarea)
+        var saveButton = document.createElement("button")
+        saveButton.classList.add("btn","col-md-1","saveBtn")
+        var iTag = document.createElement("i")
+        iTag.classList.add("fas","fa-save")
+        saveButton.appendChild(iTag)
+        row.appendChild(saveButton)
+      
+      
+      
+        
+        document.getElementById("planner").appendChild(row)
+     }
+ }
+
+ buildPlanner()
